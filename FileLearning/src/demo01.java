1ece3725e1294eb3d52db3bb946f5d7dc980c150
zhangpeishi <74777311+zhangpeishi@users.noreply.github.com>
2021-01-06 18:20:14 +0800

    javaSeniorLearning

--- FileLearning/src/demo01.java
+++ FileLearning/src/demo01.java
@@ -1,2 +1,13 @@
+import java.io.File;
+
 public class demo01 {
+    public static void main(String[] args) {
+        show();
+    }
+
+    public static void show(){
+        File f1 = new File("a.txt");
+        System.out.println(f1);
+
+    }
 }
