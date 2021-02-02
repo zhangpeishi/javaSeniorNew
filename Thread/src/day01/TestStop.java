48d56e34856c576e3a3b021f705875c22b426abb
zhangpeishi <74777311+zhangpeishi@users.noreply.github.com>
2021-02-02 19:21:03 +0800

    2021.2.2线程学习

--- Thread/src/day01/TestStop.java
+++ Thread/src/day01/TestStop.java
@@ -1,4 +1,21 @@
 package day01;
 
-public class TestStop {
+public class TestStop implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("线程一 :" + i);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        TestStop ts = new TestStop();
+        Thread t1 = new Thread(ts);
+        t1.start();
+//        for (int i = 0; i < 100; i++) {
+//            System.out.println("主线程 ：" + i);
+//       }
+    }
 }
